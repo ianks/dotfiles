f4dd517a6a48d237420038e704fb17758f263a0c
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2022-02-11 17:27:20 -0500

    Checksum yadm bootstrap

--- install.sh
+++ install.sh
@@ -2,10 +2,21 @@
 
 echo "👋 Hello! Your dotfiles will be setup momentarily..."
 
+
 # Bootstrap yadm to install dotfiles
-if [[ ! -d ~/.local/share/yadm ]]; then
+if ! command -v yadm > /dev/null; then
   echo "Installing and bootstraping YADM (for dotfile syncing)..."
-  curl -L bootstrap.yadm.io | bash -s -- https://github.com/ianks/dotfiles-repo
+
+  YADM_BOOTSTRAP_CHECKSUM=a263208a09756079e4b1f1776a656d62ddc5d945d75241cb67d6ad7f5b87a983
+  curl -o /tmp/yadm-bootstrap.sh -L https://raw.githubusercontent.com/TheLocehiliosan/yadm/master/bootstrap
+
+  if ! echo "$YADM_BOOTSTRAP_CHECKSUM  /tmp/yadm-bootstrap.sh" | shasum -a 256 -c; then
+    echo "⚠️ Checksum failed for yadm bootstrap"
+    exit 1
+  fi
+
+  sh /tmp/yadm-bootstrap.sh
+  rm /tmp/yadm-bootstrap.sh
 else
   echo "YADM config already found, now bootstraping..."
   yadm bootstrap
