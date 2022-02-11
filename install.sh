9ecd5b001bb06c5e320f0c4fa928e91e235c2c7c
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2022-02-11 17:28:56 -0500

    Use permalink for bootstrap

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if ! command -v yadm > /dev/null; then
   echo "Installing and bootstraping YADM (for dotfile syncing)..."
 
   YADM_BOOTSTRAP_CHECKSUM=a263208a09756079e4b1f1776a656d62ddc5d945d75241cb67d6ad7f5b87a983
-  curl -o /tmp/yadm-bootstrap.sh -L https://raw.githubusercontent.com/TheLocehiliosan/yadm/master/bootstrap
+  curl -o /tmp/yadm-bootstrap.sh -L https://raw.githubusercontent.com/TheLocehiliosan/yadm/3d82aff3e8140361fca18f664fffa64dfd33296e/bootstrap
 
   if ! echo "$YADM_BOOTSTRAP_CHECKSUM  /tmp/yadm-bootstrap.sh" | shasum -a 256 -c; then
     echo "⚠️ Checksum failed for yadm bootstrap"
