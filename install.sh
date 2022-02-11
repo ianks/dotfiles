4f709e691da1d0ef1aafe7dece991ee3bfe0d657
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2022-02-11 17:35:47 -0500

    Let bootstrap script determine its own shebang

--- install.sh
+++ install.sh
@@ -8,15 +8,16 @@ if ! command -v yadm > /dev/null; then
   echo "Installing and bootstraping YADM (for dotfile syncing)..."
 
   YADM_BOOTSTRAP_CHECKSUM=a263208a09756079e4b1f1776a656d62ddc5d945d75241cb67d6ad7f5b87a983
-  curl -o /tmp/yadm-bootstrap.sh -L https://raw.githubusercontent.com/TheLocehiliosan/yadm/3d82aff3e8140361fca18f664fffa64dfd33296e/bootstrap
+  curl -o /tmp/yadm-bootstrap -L https://raw.githubusercontent.com/TheLocehiliosan/yadm/3d82aff3e8140361fca18f664fffa64dfd33296e/bootstrap
 
-  if ! echo "$YADM_BOOTSTRAP_CHECKSUM  /tmp/yadm-bootstrap.sh" | shasum -a 256 -c; then
+  if ! echo "$YADM_BOOTSTRAP_CHECKSUM  /tmp/yadm-bootstrap" | shasum -a 256 -c; then
     echo "⚠️ Checksum failed for yadm bootstrap"
     exit 1
   fi
 
-  sh /tmp/yadm-bootstrap.sh
-  rm /tmp/yadm-bootstrap.sh
+  chmod +x /tmp/yadm-bootstrap
+  /tmp/yadm-bootstrap
+  rm /tmp/yadm-bootstrap
 else
   echo "YADM config already found, now bootstraping..."
   yadm bootstrap
