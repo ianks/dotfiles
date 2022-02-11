13aca60bc3b26a7636e80b9014fe00fca9167706
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2022-02-11 17:07:32 -0500

    Init commit

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ echo "👋 Hello! Your dotfiles will be setup momentarily..."
 
 # Bootstrap yadm to install dotfiles
 if [[ ! -d ~/.local/share/yadm ]]; then
-  echo "Installing yadm (for dotfile syncing)"
+  echo "Installing and bootstraping YADM (for dotfile syncing)..."
   curl -L bootstrap.yadm.io | bash -s -- https://github.com/ianks/dotfiles-repo
+else
+  echo "YADM config already found, now bootstraping..."
+  yadm bootstrap
 fi
+
+echo "✅ Done!"
