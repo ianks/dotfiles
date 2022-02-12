cbfd9b376482373020c22020a2d736fa5e007fd9
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2022-02-11 21:16:25 -0500

    Fix install script

--- install.sh
+++ install.sh
@@ -7,14 +7,13 @@ YADM_CHECKSUM="9c51f08b9be8658708b718c00c93024b122d83b6a4244836cb6d194064160d18"
 DOTFILES_REPO="https://github.com/ianks/dotfiles-repo"
 
 install_yadm() {
-
-  if command -v yadm; then
+  if command -v yadm > /dev/null; then
     echo "Detected installed yadm, no need to install"
   else
     echo "Installing and bootstraping YADM (for dotfile syncing)..."
     curl -so /tmp/yadm "https://raw.githubusercontent.com/TheLocehiliosan/yadm/$YADM_VERSION/yadm"
 
-    if ! echo "$YADM_CHECKSUM  /usr/local/bin/yadm" | shasum -a 256 -c; then
+    if ! echo "$YADM_CHECKSUM  /tmp/yadm" | shasum -a 256 -c; then
       echo "⚠️ YADM checksum mismatch, exiting"
       exit 1
     fi
@@ -26,6 +25,6 @@ install_yadm() {
 
 echo "👋 Hello! Your dotfiles will be setup momentarily..."
 install_yadm
-yadm clone "$DOTFILES_REPO"
+[[ ! -d "$HOME/.local/share/yadm/repo.git" ]] && yadm clone "$DOTFILES_REPO"
 chmod +x "$HOME/.config/yadm/bootstrap" && yadm bootstrap
 echo "✅ Done!"
