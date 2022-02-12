6cfc432f7d0c00378faf9f12011a787fd19e33d4
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2022-02-11 20:57:30 -0500

    More efficient yadm download

--- install.sh
+++ install.sh
@@ -2,23 +2,25 @@
 
 set -e
 
+YADM_VERSION="3.1.1"
+YADM_CHECKSUM="9c51f08b9be8658708b718c00c93024b122d83b6a4244836cb6d194064160d18"
 DOTFILES_REPO="https://github.com/ianks/dotfiles-repo"
 
 install_yadm() {
+
   if command -v yadm; then
     echo "Detected installed yadm, no need to install"
   else
     echo "Installing and bootstraping YADM (for dotfile syncing)..."
-    if command -v apt-get > /dev/null; then
-      sudo apt-get install -y yadm
-    elif command -v brew; then
-      brew install yadm
-    elif command -v yay; then
-      yay -Syu yadm-git
-    else
-      echo "⚠️ Could not find a suitable package manager for yadm"
+    curl -so /tmp/yadm "https://raw.githubusercontent.com/TheLocehiliosan/yadm/$YADM_VERSION/yadm"
+
+    if ! echo "$YADM_CHECKSUM  /usr/local/bin/yadm" | shasum -a 256 -c; then
+      echo "⚠️ YADM checksum mismatch, exiting"
       exit 1
     fi
+
+    chmod +x /tmp/yadm
+    sudo mv /tmp/yadm /usr/local/bin/
   fi
 }
 
